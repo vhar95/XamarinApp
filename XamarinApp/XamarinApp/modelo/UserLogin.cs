af02fc0552751995e1d27cb232aff559a28b6531
vhar95 <varh95@gmail.com>
2018-02-14 10:55:27 +0100

    Login

---
+++ XamarinApp/XamarinApp/modelo/UserLogin.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinApp.modelo
+{
+    public class UserLogin
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("Correo")]
+        public string Correo { get; set; }
+        [JsonProperty("Pass")]
+        public string Pass { get; set; }
+        [JsonProperty("Nombre")]
+        public string Nombre { get; set; }
+    }
+}
--- XamarinApp/XamarinApp/paginas/LoginUser.xaml.cs
+++ XamarinApp/XamarinApp/paginas/LoginUser.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,17 +10,35 @@ using Xamarin.Forms.Xaml;
 
 namespace XamarinApp.paginas
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class LoginUser : ContentPage
-	{
-		public LoginUser ()
-		{
-			InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginUser : ContentPage
+    {
+        private const string url = "http://localhost:8000/api/usuarios/login/";
+        private HttpClient _Client = new HttpClient();
+
+        public LoginUser()
+        {
+            InitializeComponent();
+        }
 
         private async void Principal_M(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new paginas.MenuPrincipal());
+
+            var response = await _Client.GetAsync(url + EntryCorreo.Text + "/" + EntryPassword.Text);
+
+            var code = response.IsSuccessStatusCode;
+            System.Diagnostics.Debug.WriteLine(code);
+
+            if (code != false)
+            {
+                
+                await DisplayAlert("Login Correcto", "Logeado", "Ok", "Cancelar");
+                await Navigation.PushAsync(new paginas.MenuPrincipal());
+            }
+            else
+            {
+                await DisplayAlert("Error Logueando", "Correo o Contraseña incorrectos o no existe", "Ok", "Cancelar");
+            }
         }
     }
 }
