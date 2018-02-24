b5de53e24e9eeaee0caab5e22b24ea9571568b3b
vhar95 <varh95@gmail.com>
2018-02-24 16:38:19 +0100

    vuelta

--- XamarinApp/XamarinApp/modelo/CurseClassesView.cs
+++ XamarinApp/XamarinApp/modelo/CurseClassesView.cs
@@ -9,19 +9,7 @@ namespace XamarinApp.modelo
     public class CurseClassesView: CurseClasses
     {
 
-        private CurseClassesView _selectedItem { get; set; }
-        public CurseClassesView SelectedItem
-        {
-            get
-            {
-                return _selectedItem;
-            }
-            set
-            {
-                _selectedItem = value;
-                App.Current.MainPage.Navigation.PushAsync(new paginas.Prueba(_selectedItem));
-            }
-        }
+        
 
         public CurseClassesView()
         {
